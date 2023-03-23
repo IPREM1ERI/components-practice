b35115844496925ad6e30fb056e62281e2287c2a
davidv <davidvazquezgit@gmail.com>
2023-03-23 12:32:39 -0700

    work on button component

---
+++ src/Button.jsx
@@ -0,0 +1,17 @@
+
+
+function Button({ 
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+}) {
+  
+  return <button>{children}</button>
+}
+
+export default Button;
