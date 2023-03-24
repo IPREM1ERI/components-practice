2b7eea8eb117ea6c9b1c1acc077d926bac58ee4d
davidv <davidvazquezgit@gmail.com>
2023-03-24 11:20:21 -0700

    refactor file structure

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,7 +12,7 @@ function Button({
   rounded,
   ...rest
 }) {
-    let classes = className('flex items-center px-3 py-1.5 border', {
+    let classes = className(rest.className, 'flex items-center px-3 py-1.5 border', {
       'border-blue-600 bg-blue-500 text-white': primary,
       'border-gray-900 bg-gray-800 text-white': secondary,
       'border-green-600 bg-green-500 text-white': success,
